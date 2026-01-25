c126d44e3719d25f488555cb6abd6cdd9fd2b9a7
Mario Rogério Gimenez <mariocftv@gmail.com>
2026-01-25 15:55:14 -0300

    MVP runtime safe - desativar admin e supabase

--- app/admin/credit-costs/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CreditCostsPage() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Créditos</h1>
-      <p>Módulo de créditos desativado no MVP.</p>
-    </div>
-  )
-}
--- components/HeaderCredits.tsx
+++
@@ -1,5 +0,0 @@
-// MVP: HeaderCredits desativado (sem Supabase)
-
-export default function HeaderCredits() {
-  return null
-}
